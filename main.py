ee58a0a8cb994be4f4583b5ca2963e5d478cab9a
Nirwa22 <nyusra.msai23seecs@seecs.edu.pk>
2024-10-05 02:26:22 +0500

    All four routes added

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ import os
 import uuid
 
 
-load_dotenv()
-api_key = os.getenv("SECRET_API_KEY")
-
 app = FastAPI()
-
-with open("user.json", "r") as json_file:
-    Users_data = json.load(json_file)
 
 
 class Item(BaseModel):
     Name: str | None = None
     Age: int | None = None
     Gender: str | None = None
-    id: str | None = None
+    id: str | int | None = None
+
+
+load_dotenv()
+api_key = os.getenv("SECRET_API_KEY")
+
+
+with open("user.json", "r") as json_file:
+    Users_data = json.load(json_file)
 
 
 @app.get("/")
@@ -32,25 +34,21 @@ async def user_data():
     return Users_data
 
 
-@app.post("/add")
-def add(item: Item):
-    return item
-
-
 @app.post("/Add_Users_data")
 async def add_data(item: Item, request: Request):
     api = request.headers.get("Authorization")
-    user_info = {"Name": item.Name, "Age": item.Age, "Gender": item.Gender}
     if api and api != api_key:
         return "Unauthorized access"
     elif not api:
         return "API_Key needed"
     elif api == api_key:
-        session_id = uuid.uuid4()
-        user_info["id"] = str(session_id)
-        with open("user.json", "a") as newuser_file:
-            json.dump(user_info, newuser_file, indent=4)
+        session_id = str(uuid.uuid4())
+        user_info = {"id": session_id, "Name": item.Name, "Age": item.Age, "Gender": item.Gender}
+        Users_data.append(user_info)
+        with open("user.json", "w") as new_user_file:
+            json.dump(Users_data, new_user_file, indent=4)
         return f"Authorization Successful: Your session_id is {session_id}"
+
 
 @app.post("/Get_User_Information")
 async def get_data(item: Item, request: Request):
@@ -63,7 +61,7 @@ async def get_data(item: Item, request: Request):
         with open("user.json", "r") as file:
             users_information = json.load(file)
         for i in users_information:
-            if i["id"] == item[4]:
+            if i["id"] == item.id:
                 return {"Name": i["Name"], "Age": i["Age"], "Gender": i["Gender"]}
                 break
         return {"Error Occured: Session_id does not exist"}
