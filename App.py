cbd0879d9bdc6a6c89f7db54706075b92d3b66e0
Nirwa22 <nyusra.msai23seecs@seecs.edu.pk>
2024-10-04 20:01:34 +0500

    new files added

--- App.py
+++ App.py
@@ -1,34 +1,36 @@
 from fastapi import FastAPI
-from typing import Union
 from fastapi.params import Body
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import json
 import os
 
+
 load_dotenv()
 api_key = os.getenv("SECRET_API_KEY")
+
 app = FastAPI()
 
 with open("user.json", "r") as json_file:
     Users_data = json.load(json_file)
 
 
+class Item(BaseModel):
+    name: str
+    price: float
+
+
 @app.get("/")
-def home():
-    return {"Home Route"}
+async def home():
+    return {"Welcome"}
 
 
 @app.get("/Users_data")
-def user_data():
+async def user_data():
     return Users_data
 
 
-@app.post("/post")
-def post():
-    return {"Message": "Post Created"}
 @app.post("/Add_Users_data")
-def add_data(turtle: dict = Body(...)):
-    return {turtle["id"]}
-
-
+async def add_data(item: Item):
+    return item
 
